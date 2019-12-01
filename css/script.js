9aac42f01feba2603404db9f5c82c19f96f90274
Evans-o <evansopindi@yahoo.com>
2019-12-01 20:54:48 +0300

    added initial structures

--- css/script.js
+++ css/script.js
@@ -17,19 +17,3 @@
     <input id="date" type="text" name="input" placeholder="YYYY-MM-DD" pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))" title="Enter a date in this format YYYY-MM-DD" required />
 
     <p id="myId"></p>
-<script>
-    function myDate() {
-    var a = new Date("11/23/2019");
-    var days = new Array(7);
-    days[0] = "Name: kwasi, Born:Sunday";
-    days[1] = "Name: Kwado, Born:Monday";
-    days[2] = "Name: Kwabena, Born:Teusday";
-    days[3] = "Name: Kwaku, Born:Wenesday";
-    days[4] = "Name: Yaw, Born:Thursday";
-    days[5] = "Name: Kofi, Born:Friday";
-    days[6] = "Name: Kwamo, Born:Saturday";
-    var r = alert(days[a.getDay()]);
-    ocument.getElementById("date").innerHTML = r;
-  }
-
-</script>
